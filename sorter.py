85c5f820beeb01a06bebabcaafb45c9081be6e5d
TeachMeTW <teachmetheways652@gmail.com>
2023-11-19 19:09:48 -0800

    Set temporary lookback

--- sorter.py
+++ sorter.py
@@ -26,24 +26,53 @@ image_label.pack()
 info_label = tk.Label(root, text="Choose a folder to start")
 info_label.pack()
 
+# Label for displaying the statistics
+stats_label = tk.Label(root, text="")
+stats_label.pack()
+
 # List to hold the categorization data and the image list
 categorizations = {}
 image_list = []
 current_image_index = 0
+filtered_image_list = []
+review_mode = False
+current_category = 'unsorted'
 
-# Function to update info label
+# Function to update info label based on the category
 def update_info_label():
-    image_name = os.path.basename(image_list[current_image_index])
+    if review_mode:
+        image_name = os.path.basename(filtered_image_list[current_image_index])
+        progress = f"{current_image_index + 1} of {len(filtered_image_list)}"
+    else:
+        image_name = os.path.basename(image_list[current_image_index])
+        progress = f"{current_image_index + 1} of {len(image_list)}"
+    
     category = categorizations.get(image_name, "Not categorized")
-    info_label.config(text=f"Image: {image_name} | Category: {category} | Progress: {current_image_index + 1} of {len(image_list)}")
+    stats = calculate_stats()
+    info_label.config(text=f"Image: {image_name} | Category: {category} | Progress: {progress}")
+    stats_label.config(text=f"Stats: {stats}")
+
+# Calculate statistics
+def calculate_stats():
+    total_categorized = len(categorizations)
+    counts = {cat: len([name for name in categorizations.values() if name == cat]) for cat in categories.values()}
+    # Check if there are any categorized images to avoid division by zero
+    if total_categorized > 0:
+        percentages = {cat: (count / total_categorized * 100) for cat, count in counts.items()}
+    else:
+        percentages = {cat: 0 for cat in categories.values()}
+    stats_string = " | ".join([f"{cat}: {counts[cat]} ({percentages[cat]:.2f}%)" for cat in categories.values()])
+    return stats_string
 
 # Function to choose folder and load images
 def choose_folder_and_load_images():
-    global image_list, current_image_index
+    global image_list, current_image_index, review_mode
+    review_mode = False
+    current_image_index = 0
     folder_selected = filedialog.askdirectory(initialdir=os.getcwd())  # Start in the current working directory
     if folder_selected:
         load_images(folder_selected)
-        load_progress('categorizations.json')
+        load_progress('FullData.json')
         display_current_image()
         update_info_label()
 
@@ -55,15 +84,17 @@ def load_images(folder_path):
 
 # Display the current image
 def display_current_image():
-    global current_image_index, image_list
-    if 0 <= current_image_index < len(image_list):
+    global current_image_index, image_list, filtered_image_list, review_mode
+    if review_mode:
+        img_path = filtered_image_list[current_image_index]
+    else:
         img_path = image_list[current_image_index]
-        img = Image.open(img_path)
-        img.thumbnail((800, 600), Image.ANTIALIAS)  # Resize if needed
-        img_tk = ImageTk.PhotoImage(img)
-        image_label.config(image=img_tk)
-        image_label.image = img_tk  # Keep a reference
-        update_info_label()
+    img = Image.open(img_path)
+    img.thumbnail((800, 600), Image.ANTIALIAS)  # Resize if needed
+    img_tk = ImageTk.PhotoImage(img)
+    image_label.config(image=img_tk)
+    image_label.image = img_tk  # Keep a reference
+    update_info_label()
 
 # Load progress from the JSON file
 def load_progress(file_path):
@@ -83,24 +114,37 @@ def load_progress(file_path):
 
 # Handle key press event
 def categorize_image(event):
-    global current_image_index, categorizations, image_list
+    global current_image_index, categorizations, image_list, review_mode, filtered_image_list
     if event.char in categories:
-        image_name = os.path.basename(image_list[current_image_index])
+        if review_mode:
+            image_name = os.path.basename(filtered_image_list[current_image_index])
+        else:
+            image_name = os.path.basename(image_list[current_image_index])
         categorizations[image_name] = categories[event.char]
-        save_categorizations('categorizations.json')
-        move_to_next_image()
-
+        save_categorizations('FullData.json')
+        if review_mode:
+            filtered_image_list.pop(current_image_index)
+            if current_image_index >= len(filtered_image_list):
+                current_image_index = len(filtered_image_list) - 1
+        else:
+            move_to_next_image()
 
 # Move to next image
 def move_to_next_image():
-    global current_image_index
-    current_image_index = min(current_image_index + 1, len(image_list) - 1)
+    global current_image_index, image_list, review_mode, filtered_image_list
+    if review_mode:
+        current_image_index = min(current_image_index + 1, len(filtered_image_list) - 1)
+    else:
+        current_image_index = min(current_image_index + 1, len(image_list) - 1)
     display_current_image()
 
 # Move to previous image
 def move_to_previous_image():
-    global current_image_index
-    current_image_index = max(current_image_index - 1, 0)
+    global current_image_index, review_mode, filtered_image_list
+    if review_mode:
+        current_image_index = max(current_image_index - 1, 0)
+    else:
+        current_image_index = max(current_image_index - 1, 0)
     display_current_image()
 
 # Save categorizations to a JSON file
@@ -110,7 +154,7 @@ def save_categorizations(file_path):
 
 # Save progress when the application closes
 def on_closing():
-    save_categorizations('categorizations.json')
+    save_categorizations('FullData.json')
     root.destroy()
 
 # Bind keys to the categorize_image function and arrow key functions
@@ -125,6 +169,29 @@ root.protocol("WM_DELETE_WINDOW", on_closing)
 # Button for choosing a folder
 choose_folder_button = tk.Button(root, text="Choose Folder", command=choose_folder_and_load_images)
 choose_folder_button.pack()
+
+# New buttons to navigate by category
+yes_button = tk.Button(root, text="Review Yes", command=lambda: filter_images_by_category('yes'))
+no_button = tk.Button(root, text="Review No", command=lambda: filter_images_by_category('no'))
+unsure_button = tk.Button(root, text="Review Unsure", command=lambda: filter_images_by_category('unsure'))
+unsorted_button = tk.Button(root, text="Review Unsorted", command=lambda: filter_images_by_category('unsorted'))
+
+yes_button.pack()
+no_button.pack()
+unsure_button.pack()
+unsorted_button.pack()
+
+# New function to filter images by category
+def filter_images_by_category(category):
+    global filtered_image_list, current_image_index, current_category, review_mode
+    review_mode = True
+    current_category = category
+    if category == 'unsorted':
+        filtered_image_list = [img for img in image_list if os.path.basename(img) not in categorizations]
+    else:
+        filtered_image_list = [img for img in image_list if categorizations.get(os.path.basename(img), 'Not categorized') == category]
+    current_image_index = 0
+    display_current_image()
 
 # Start the GUI loop
 root.mainloop()
